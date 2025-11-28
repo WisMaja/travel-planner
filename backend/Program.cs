8b29f06a04c8e3eb32bfa516f4b01c3489b3deed
Maja <wisniewska.maja03@gmail.com>
2025-11-28 12:46:54 +0100

    prawidłowe połaczenie z baza i otwieranie swaggera

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,25 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Connection;
+using backend.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Dodanie obsługi OpenAPI (dla Swagger)
 builder.Services.AddOpenApi();
 
-// Swagger + endpoint explorer
+// Konfiguracja Swagger - dokumentacja API
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+//DbContext - połaczenie z baza
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
-// Swagger w DEV (UI na /swagger)
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+// Konfiguracja Swaggera
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-
-app.MapGet("/api/info", () => new { message = "ok", time = DateTime.UtcNow });
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Travel Planner API V1");
+    c.RoutePrefix = string.Empty; // Swagger dostępny na głównej stronie "/"
+});
+// Endpointy testowe
+app.MapTestEndpoints();
 
 
 app.Run();
-
