ce2165d8ace8f30ef7eee427b4ec99633727ef10
Maja <wisniewska.maja03@gmail.com>
2025-11-30 12:04:02 +0100

    plan-bookings do dorobienia jeszcze

--- frontend/src/app/pages/plan-bookings-edit/plan-bookings-edit.ts
+++ frontend/src/app/pages/plan-bookings-edit/plan-bookings-edit.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { SharedImports } from '../../shared/shared-imports/shared-imports';
+import { GoogleMapComponent, MapPlace } from '../components/maps/google-map/google-map';
+import { TopBarPlan } from '../components/plan/top-bar-plan/top-bar-plan';
+import { Button } from '../components/ui/buttons/button/button';
 
 @Component({
   selector: 'app-plan-bookings-edit',
-  imports: [SharedImports],
+  imports: [SharedImports, GoogleMapComponent, TopBarPlan, Button],
   templateUrl: './plan-bookings-edit.html',
   styleUrl: './plan-bookings-edit.scss',
 })
 export class PlanBookingsEdit {
-
+  // Mapa - pusta lista miejsc (rezerwacje nie mają miejsc do wyświetlenia)
+  get placesForMap(): MapPlace[] {
+    return [];
+  }
 }
